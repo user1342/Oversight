3a1663cc6581fecdaa153e4444a671da345136a0
James Stevenson <28961606+user1342@users.noreply.github.com>
2024-11-06 20:53:30 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='oversight',
-    version='0.12',
+    version='0.13',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
